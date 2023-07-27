07effa72d9a7f22015b93d3be543cd9df6a87f85
myinfo <chetraj0101@gmail.com>
2023-07-27 14:12:29 -0500

    added new file with select option for buying list apps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'buylist';
+export class AppComponent implements OnInit {
+  myForm: FormGroup | any;
+
+  options = [
+    { value: '', label: 'Choose the Lists' },
+    { value: 'clothingAcces', label: 'Clothing And Accessory' },
+    { value: 'entertainment', label: 'Entertainment And Hobbies' },
+    { value: 'grocery', label: 'Grocery Items' },
+    { value: 'healthWellness', label: 'Health And Wellness' },
+    { value: 'houseHold', label: 'Household Items' },
+    { value: 'personalCare', label: 'Personal Care' },
+    { value: 'subscription', label: 'Subscription Lists' },
+    { value: 'travel', label: 'Travel Lists' }
+  ];
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.myForm = this.formBuilder.group({
+      selectedList: [''] // Default value for the select element
+    });
+  }
+
 }

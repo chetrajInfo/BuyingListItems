cdcdfad78fc97ff77b2d2732fcf0567b1b3abbc3
myinfo <chetraj0101@gmail.com>
2023-08-02 22:30:05 -0500

    complete the register and loin validation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { MainpageComponent } from './mainpage/mainpage.component';
+import { RegisterComponent } from './register/register.component';
 
 // Define your routes here
 const routes: Routes = [
   { path: '', component: LoginComponent }, 
   { path: 'mains', component:MainpageComponent},
+  { path: 'register', component:RegisterComponent},
   // Add other routes as needed
 ];
 
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+
+  register!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.register = this.formBuilder.group({
+      username: ['', Validators.required], // Username is required
+      email: ['', [Validators.required, Validators.email]], // Email is required and must be a valid email
+      passwordOne: ['', Validators.required], // Password is required
+      passwordTwo: ['', Validators.required], // Re-type Password is required
+    }, {
+      validators: this.passwordsMatchValidator // Custom validator to check if passwords match
+    });
+  }
+
+  // Custom validator function to check if passwords match
+  passwordsMatchValidator(group: FormGroup) {
+    const passwordOne = group.get('passwordOne')?.value;
+    const passwordTwo = group.get('passwordTwo')?.value;
+
+    // Check if both passwords are the same
+    return passwordOne === passwordTwo ? null : { passwordsNotMatch: true };
+  }
 
 }
